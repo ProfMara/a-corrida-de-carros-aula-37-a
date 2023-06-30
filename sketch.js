2df443f3a02430023ec2d30e313c11bae23cadbd
ProfMara <94469709+ProfMara@users.noreply.github.com>
2023-06-30 09:36:33 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,7 +14,7 @@ function preload() {
   cenario = loadImage("./assets/planodefundo.png");
   carimg1 = loadImage("car1.png");
   carimg2 = loadImage("car2.png");
-  pista = loadImage("pista.png");
+  pista = loadImage("track.png");
 }
 
 function setup() {
